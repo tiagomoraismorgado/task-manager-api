75d2fa1edbe8ba5c5c9a61d477d4d064c57fc02a
Cyrine Ben Hassen <syrinebenhassen09@gmail.com>
2025-03-01 14:33:19 +0100

    Commit add notif

---
+++ models/Notification.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  task: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task',
+    required: false,
+  },
+  isRead: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Notification', notificationSchema);
