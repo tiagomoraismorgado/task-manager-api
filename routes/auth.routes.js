6db97fd7b7c04df71d9e9348b98e837417f854ee
Fer-012 <aouachriferdaoues@gmail.com>
2025-03-05 11:22:13 +0100

    Add invitation model, refactor routes, and implement user authentication

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const User = require("../models/User");
 
 const router = express.Router();
 
