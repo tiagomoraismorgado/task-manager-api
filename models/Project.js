b034eca6d95c23c980b4c1eac924cf759b474655
Fer-012 <aouachriferdaoues@gmail.com>
2025-03-08 20:49:21 +0100

    Remove collaborator handling from project creation and update UI for project form

--- models/Project.js
+++ models/Project.js
@@ -8,7 +8,6 @@ const projectSchema = new mongoose.Schema({
     priority: { type: String, enum: ["Low", "Medium", "High"], default: "Medium" },
     status: { type: String, enum: ["Ongoing", "Completed", "Cancelled"], default: "Ongoing" },
     admin: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    collaborators: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Reference User model
     tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }]
 });
 
