4e9ea406d57634f5b7582a82b1f476467d00ace7
Cyrine Ben Hassen <syrinebenhassen09@gmail.com>
2025-03-09 16:37:14 +0100

    Refactor project details view: change route to GET, update rendering logic, and enhance error handling

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -39,18 +39,18 @@ router.get("/all", authMiddleware, async (req, res) => {
   }
 });
 
-//view project details
-router.post("/project/:id", authMiddleware, async (req, res) => {
-    try {
-      const project = await Project.findById(req.params.id);
-      if (!project) {
-        return res.status(404).json({ message: "Project not found" });
-      }
-      res.render('projectDetails', { project: project }); // Rendre une vue avec les détails du projet
-    } catch (error) {
-      res.status(500).json({ message: "Error fetching project", error: error.message });
+//view details project
+router.get("/project/:id", authMiddleware, async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
     }
-  });
+    res.json(project); // Renvoyer les données du projet au format JSON
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching project", error: error.message });
+  }
+});
 
 
 // GET /api/projects/:id
